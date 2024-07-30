2371e18cc61bab51ab4cb8a41b89508286f4d793
Pushan <pushanban@gmail.com>
2024-07-31 00:22:38 +0530

    arhn

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="layout">
+    <Header />
+    <transition name="fade" tag="div" appear>
+      <main>
+        <NuxtPage />
+      </main>
+    </transition>
+    <Footer class="footer" />
   </div>
 </template>
+
+<script>
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default {
+  components: {
+    Footer,
+    Header,
+  },
+};
+</script>
+
+<style>
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+</style>
